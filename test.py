212076f8c7b22061347cfe32ab7b0f00eacd70d3
bingxing <bingxing@yiran.com>
2016-04-10 10:07:46 +0800

    start

---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import csv
+
+with open('../1.csv', 'rb') as csvfile, open('../2.csv', 'wb') as writefile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    s = set([])
+    for row in spamreader:
+        str = ",".join(row).replace("?", "")
+        if str.find("？") == None:
+            continue
+        s.add(str)
+
+    spamwriter = csv.writer(writefile)
+    for str in s:
+        # print si
+        spamwriter.writerow(str.split(','))
+
+
+
+
+
---
+++ tutorial/spiders/baike_spider.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+from tutorial.items import DmozItem
+
+class BaikeSpider(scrapy.Spider):
+    name = "baike"
+    allowed_domains = ["baike.baidu.com"]
+    start_urls = [
+        "http://www.baike.baidu.com/item/麻黄",
+    ]
+
+
+    # def parse(self, response):
+    #     for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+    #         print response.url, href.extract()
+    #         url = response.urljoin(href.extract())
+    #         yield scrapy.Request(url, callback=self.parse_dir_contents)
+    #
+    # def parse_dir_contents(self, response):
+    #     for sel in response.xpath('//ul/li'):
+    #         item = DmozItem()
+    #         item['title'] = sel.xpath('a/text()').extract()
+    #         item['link'] = sel.xpath('a/@href').extract()
+    #         item['desc'] = sel.xpath('text()').extract()
+    #         yield item
+
+
+    def parse(self, response):
+        suffix1 = "/descendant-or-self::*/text()"
+        suffix2 = "/text()"
+        suffix3 = "/descendant-or-self::*[not(self::span) and not(@class='image-link') and not(self::sup)]/text()"
+        remove_white = lambda s: re.sub(r"\s+", "", s, flags=re.UNICODE)
+
+        #introduce
+        introduce = response.xpath("//div[@class='lemma-summary']/div[@class='para']"+suffix1).extract()
+        print "".join(introduce).encode('utf-8')
+
+        #basicInfo
+        f_num = lambda n : "[" + str(n) + "]"
+        basic_key_left = "//dl[@class='basicInfo-block basicInfo-left']/dt[@class='basicInfo-item name']"
+        basic_key_right = "//dl[@class='basicInfo-block basicInfo-right']/dt[@class='basicInfo-item name']"
+        basic_value_left = "//dl[@class='basicInfo-block basicInfo-left']/dd[@class='basicInfo-item value']"
+        basic_value_right = "//dl[@class='basicInfo-block basicInfo-right']/dd[@class='basicInfo-item value']"
+
+        def basic_kv(key, value):
+            upper_bound = 100
+            for i in range(1, upper_bound):
+                key_data = response.xpath(key + f_num(i) + suffix1).extract()
+                value_data = response.xpath(value + f_num(i) + suffix1).extract()
+                if key_data == []:
+                    break
+                print  "".join(key_data).replace(u"\xa0", "").strip().encode('utf-8')
+                print  "".join(value_data).replace(u"\xa0", "").strip().encode('utf-8')
+        basic_kv(basic_key_left, basic_value_left)
+        basic_kv(basic_key_right, basic_value_right)
+        print "\n"
+
+        #detailPara
+        f2 = lambda n : "//div[@class='para-title level-2'][" + str(n) + "]/h2[@class='title-text']"
+        f3 = lambda n : "//div[@class='para-title level-2'][" + str(n) + "]/following::*/descendant-or-self::*" \
+            "[(@class='para' or @class='title-text or self::a') and not(self::h2) and " \
+            "count(preceding::div[@class='para-title level-2'])=" + str(n) + "]"
+        f_title = lambda n : "(//div[@class='para-title level-2'][" + str(n) + "]/following::*/descendant-or-self::*" \
+            "[self::h3 and count(preceding::div[@class='para-title level-2'])=" + str(n) + "])"
+        f_data = lambda n, m : "(" + f_title(n,m) + "/parent::*/following::*/descendant-or-self::*" \
+            "[(@class='para' or self::a) and not(self::h2) and not(self::h3)] intersect " + \
+            f_title(n,m+1) + "/parent::*/preceding::*/descendant-or-self::*" + ")"
+        key_num = int(float("".join(response.xpath("count(//div[@class='para-title level-2'])").extract())))
+        for i in range(1, key_num+1):
+            key_data = response.xpath(f2(i) + suffix2).extract()
+            value_data = response.xpath(f3(i) + suffix3).extract()
+            title_num = int(float("".join(response.xpath("count(" + f_title(i) + ")").extract())))
+            print remove_white("".join(key_data)).encode('utf-8')
+            print remove_white("".join(value_data)).encode('utf-8')
+            print title_num
+            for j in range(1, title_num+1):
+                title_data = response.xpath(f_title(i) + f_num(j) +  suffix2).extract()
+                #value_data = response.xpath(f_data(i,j) + suffix3).extract()
+                if (title_data == []):
+                    break
+                print "".join(title_data).encode('utf-8')
+                #print "".join(value_data).encode('utf-8')
+            print "\n"
+
