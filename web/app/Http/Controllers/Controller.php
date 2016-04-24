24f5d80c1be0d8961988f7cd1eda3aee74f1fbb3
bingxing <lisjiechn@gmail.com>
2016-04-24 19:27:49 +0800

    web finished

--- web/app/Http/Controllers/Controller.php
+++ web/app/Http/Controllers/Controller.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected $request = '';
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
 }
--- web/resources/views/ask.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "very_good";
