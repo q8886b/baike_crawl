e85bb5184326eabac7490e3b4809b91d31be9cf1
bingxing <lisjiechn@gmail.com>
2016-05-20 02:31:36 +0800

    update

--- tutorial/spiders/baike_url_spider.py
+++ tutorial/spiders/baike_url_spider.py
@@ -15,34 +15,25 @@ class BaikeUrlSpider(scrapy.Spider):
 
     valid_tags = ['中药', '中药材', '中成药', '草药', '中草药', '方剂', '药材', '药方', '药用植物']
 
-    # start_urls = []
-    # for valid_tag in valid_tags:
-    #     start_urls.append("http://baike.baidu.com/taglist?tag=" + valid_tag)
-    # temp_list = []
-    # for url in start_urls:
-    #     for i in range(0, 76):
-    #         sub = url + "&offset=" + str(i*10)
-    #         temp_list.append(sub)
-    # start_urls = temp_list
+    file = open("data/known.url")
+    known_urls = set([url.strip() for url in file])
+    file.close()
 
-    # start_urls = ['http://baike.baidu.com/view/565498.htm']
-
-    # file = open("data/valid_items.txt")
-    # start_urls = ["http://www.baike.baidu.com/item/" + item.strip() for item in file.readlines()]
-    # file.close()
-    # known_urls = set(start_urls)
-
-    file = open("data/exchange.txt")
+    file = open("data/valid.url")
     start_urls = [url.strip() for url in file]
     file.close()
-    known_urls = set(start_urls) # contain valid url, and invalid urls
+
+    file = open("data/valid.url")
+    valid_urls = set([url.strip() for url in file])
+    file.close()
+
 
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url, self.parse, meta={
                 'splash': {
                     'endpoint': 'render.html',
-                    'args': {'wait': 0, 'resource_timeout':1, 'image':0},
+                    'args': {'wait': 0, 'image':0},
                 }
                 #'origin_url': url
             })
@@ -55,40 +46,24 @@ class BaikeUrlSpider(scrapy.Spider):
         return False
 
     def parse(self, response):
+        url = response.meta['splash']['args']['url']
+        for href in response.xpath("//div[@class='zhixin-box']/descendant-or-self::*/a/@href").extract():
+            if href.find('http') == -1:
+                href = u'http://baike.baidu.com' + href
+            if self.url_valid(href.encode('utf-8')):
+                if href not in self.known_urls:
+                    print "new known:", href
+                    self.known_urls.add(href)
+                    yield scrapy.Request(href, self.parse_valid)
 
-        # url = response.meta['splash']['args']['url']
-        # print url
-	url = response.url
-	"""
-        omit = True
+    def parse_valid(self, response):
+        url = response.url
         tags = response.xpath("//span[@class='taglist']/text()").extract()
         for tag in tags:
             if tag.strip().encode('utf-8') in self.valid_tags:
-                if url not in self.known_urls:
-                    print url
-                    omit = False
+                print "n v:", url
                 break
-        self.known_urls.add(url)
-	"""
-
-	omit = False
-        if omit == False:
-            for href in response.xpath("//div[@class='zhixin-box']/descendant-or-self::*/a/@href").extract():
-                if href.find('http') == -1:
-                    href = u'http://baike.baidu.com' + href
-                if self.url_valid(href.encode('utf-8')):
-                    if href not in self.known_urls:
-                        print href
-                        self.known_urls.add(href.encode('utf-8'))
-			"""
-                        yield scrapy.Request(href, self.parse, meta={
-                            'splash': {
-                                'endpoint': 'render.html',
-                                'args': {'wait': 0, 'resource_timeout':1, 'image':0},
-                            }
-                        })
-			"""
-			return
+        return
 
 
 
